aba7f0ac64714b37a36f86eab42ad01a6f43ba16
obverter <ben@obverter.com>
2022-07-26 22:21:25 -0700

    Fix scraper I broke

--- automatic-scraper/scraper.py
+++ automatic-scraper/scraper.py
@@ -2,43 +2,43 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Grab trash
+
 url = "https://www.tmz.com"
 req = requests.get(url)
 doc = BeautifulSoup(req.text)
 
+
+stories = []
+for trash in doc.select("header > a > h2"):
+    raw = trash.text
+    story = {"headline": raw.strip().replace("\n", " ")}
+    print(f"processing headline: {trash}")
+    stories.append(story)
+
+
 tmz = doc.select("header a h2")
 tmz_timestamps = doc.select(".article")
-stories = pd.DataFrame(columns=["timestamp", "headline"])
-new_stories = pd.DataFrame(columns=["timestamp", "headline"])
+stories = pd.DataFrame()
+heads = []
+times = []
 paragraphs = []
 count = 0
-for _ in enumerate(tmz):
-    # Grab headline
+for trash in enumerate(tmz):
     headline = tmz[count].text
     headline = headline.replace("\n", " ")
 
-    # Grab timestamp
     timestamp = tmz_timestamps[count].text.split("PT")[-20:]
     timestamp = timestamp[0][-20:]
     timestamp = timestamp.strip()
 
-    # Prep for append
-    story = {"timestamp": [timestamp], "headline": [headline]}
+    story = {"timestamp": timestamp, "headline": headline}
 
-    # append
-    new_stories |= story
+    story = pd.DataFrame([story])
+    stories = pd.concat([stories, story], ignore_index=True)
+
+    # stories |= story
 
     count += 1
 
-try:
-    # Pull current headlines.csv
-    current = pd.read_csv("headlines.csv")
-except Exception:
-    # Init headlines.csv if it doesn't exist
-    current = pd.DataFrame(columns=["timestamp", "headline"])
 
-merge = pd.concat([current, new_stories], ignore_index=True)
-merge = merge[["timestamp", "headline"]]
-
-merge.to_csv("headlines.csv")
+stories.to_csv("headlines.csv")
