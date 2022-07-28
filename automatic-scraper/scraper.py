94c8af2a2c12be526b7cd6456eb355d15e673c67
obverter <ben@obverter.com>
2022-07-27 18:27:46 -0700

    Clean up after myself

--- automatic-scraper/scraper.py
+++ automatic-scraper/scraper.py
@@ -6,6 +6,7 @@ from datetime import *
 from bs4 import BeautifulSoup
 
 
+# This is a web scraper. It is grabbing the html from the TMZ website.
 url = "https://www.tmz.com"
 req = requests.get(url)
 doc = BeautifulSoup(req.text)
@@ -34,7 +35,7 @@ for trash in enumerate(tmz):
     timestamp = timestamp[0][-20:]
     timestamp = timestamp.strip()
 
-    # set timestamp, date, time, headlie
+    # set timestamp, date, time, headline
     story = {
         "timestamp": timestamp,
         "date": timestamp[:9],
@@ -50,44 +51,42 @@ for trash in enumerate(tmz):
         story["time"] = pd.to_datetime(story["time"], format="%I:%M %p").dt.strftime(
             "%H:%M"
         )
-    except:
+    except Exception:
         story["time"] = story["time"]
 
     story.convert_dtypes()
 
+    # Parsing the date and time into columns.
     story["year"] = pd.DatetimeIndex(story["date"]).year
     story["month"] = pd.DatetimeIndex(story["date"]).month
     story["day"] = pd.DatetimeIndex(story["date"]).day
     story["hour"] = pd.DatetimeIndex(story["time"]).hour
     story["minute"] = pd.DatetimeIndex(story["time"]).minute
-    # parse date into columns
-    # try:
-    #     story["year"] = story['date'].astype(str)[-4:]
-    # except:
-    #     story["year"] = story["year"]
 
     # append story to raw stories df to be merged with historical df
     stories = pd.concat([stories, story], ignore_index=True)
 
     count += 1
 
+# Creating a new dataframe with the columns headline, timestamp, year, month, day,
+# hour, and minute.
 stories = stories[["headline", "timestamp", "year", "month", "day", "hour", "minute"]]
-stories
 
 
+# This is reading the headlines.csv file and converting it to a dataframe.
 existing = pd.read_csv("headlines.csv")
 existing = existing.dropna(axis=1).set_index("Unnamed: 0")
 existing.convert_dtypes()
 
+# Parsing the timestamp into columns.
 existing["year"] = pd.DatetimeIndex(existing["timestamp"]).year
 existing["month"] = pd.DatetimeIndex(existing["timestamp"]).month
 existing["day"] = pd.DatetimeIndex(existing["timestamp"]).day
 existing["hour"] = pd.DatetimeIndex(existing["timestamp"]).hour
 existing["minute"] = pd.DatetimeIndex(existing["timestamp"]).minute
 
-existing
-
-
+# This is a try/except block. It is trying to read the headlines.csv file and
+# convert it to a dataframe with time-parsed columns. If it fails, it will pass.
 try:
     existing = pd.read_csv("headlines.csv")
     existing.convert_dtypes()
@@ -109,19 +108,21 @@ try:
             "minute",
         ]
     ]
-except:
+except Exception:
     pass
 
+# This is concatenating the existing dataframe with the new dataframe.
 export = pd.concat([existing, stories], ignore_index=True)
 export = export[["headline", "timestamp", "year", "month", "day", "hour", "minute"]]
 
+# Dropping duplicates and sorting the dataframe by year, month, day, hour, and
+# minute.
 export = export.drop_duplicates(subset=["timestamp"], keep="first")
 export = export.sort_values(
     by=["year", "month", "day", "hour", "minute"],
     ascending=[False, False, False, False, False],
     ignore_index=True,
 )
-export
 
-
+# Writing the dataframe to a csv file.
 export.to_csv("headlines.csv")
