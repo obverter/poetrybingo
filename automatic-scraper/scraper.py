8f0edfdea49d8f4e00caccb02b69a1126d187447
obverter <ben@obverter.com>
2022-08-02 23:18:03 -0700

    2.0 online

--- automatic-scraper/scraper.py
+++ automatic-scraper/scraper.py
@@ -1,128 +1,132 @@
+# To add a new cell, type ''
+# To add a new markdown cell, type ' [markdown]'
+
 # sourcery skip: for-index-underscore
-import contextlib
 import pandas as pd
 import requests
-from datetime import *
+from datetime import datetime
 from bs4 import BeautifulSoup
+import re
+from tqdm import tqdm
+import json
 
 
-# This is a web scraper. It is grabbing the html from the TMZ website.
 url = "https://www.tmz.com"
 req = requests.get(url)
 doc = BeautifulSoup(req.text)
 
 
-# init var headline
-tmz = doc.select("header a h2")
+raw_titles = doc.select("header > a > h2")
 
 
-# init var timestamp
-tmz_timestamps = doc.select(".article")
+headlines = []
+valid_punct = ["'", "!", "?"]
+for count, title in enumerate(tqdm(enumerate(raw_titles))):
+    print("- - -")
+    headline = raw_titles[count].get_text().strip().replace("\n", " ") + "."
+    if headline[-2] in valid_punct:
+        headline = headline[:-1]
+    print(headline)
+    headlines.append(headline)
 
 
-# main loop
-stories = pd.DataFrame()
-paragraphs = []
+def get_tags(article, position):
+    tags = doc.select("section.tag-cloud > ul > a")
+    tag_list = []
+    for tag in tags:
+        data = json.loads(tag["data-context"])
+        if data["pos"] == position:
+            tag_list.append(tag.text.strip())
+    return tag_list
+
+
+def break_timestamp(article, position):
+    timestamps = doc.select(".article")
+    timestamp = timestamps[position].text.split("PT")[-20:]
+    timestamp = timestamp[0][-20:]
+    timestamp = timestamp.strip()
+    calendar = timestamp[:9]
+    clock = timestamp[-8:].strip()
+    date_a_frame = pd.DataFrame()
+    year = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").year
+    month = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").month
+    day = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").day
+    hour = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").hour
+    minute = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").minute
+    datetimes = {
+        "calendar": calendar,
+        "clock": clock,
+        "year": year,
+        "month": month,
+        "day": day,
+        "hour": hour,
+        "minute": minute,
+    }
+
+    meta = [datetimes]
+    return datetimes
+
+
+break_timestamp(raw_titles, 3)
+
+
+# init dataframe of tags
+tags = doc.select("section.tag-cloud > ul > a")
+
+valid_punct = ["'", "!", "?"]
+
+dicts = []
+
 count = 0
+for article_position, article in enumerate(raw_titles, start=1):
+    headline = raw_titles[count].get_text().strip().replace("\n", " ") + "."
+    if headline[-2] in valid_punct:
+        headline = headline[:-1]
+    tag_list = get_tags(article, count + 1)
 
-for trash in enumerate(tmz):
-    # grab headline text
-    headline = tmz[count].text
-    headline = headline.replace("\n", " ")
-
+    datetimes = break_timestamp(article, count)
+    timestamps = doc.select(".article")
     # grab, clean timestamp
-    timestamp = tmz_timestamps[count].text.split("PT")[-20:]
+    timestamp = timestamps[count].text.split("PT")[-20:]
     timestamp = timestamp[0][-20:]
     timestamp = timestamp.strip()
 
-    # set timestamp, date, time, headline
-    story = {
-        "timestamp": timestamp,
-        "date": timestamp[:9],
-        "time": timestamp[-8:].strip(),
+    article_dict = {
         "headline": headline,
+        "tags": tag_list,
+        "timestamp": timestamp,
+        "year": datetimes["year"],
+        "month": datetimes["month"],
+        "day": datetimes["day"],
+        "hour": datetimes["hour"],
+        "minute": datetimes["minute"],
     }
-
-    # convert to df
-    story = pd.DataFrame([story])
-
-    # convert story 12AP to 24
-    try:
-        story["time"] = pd.to_datetime(story["time"], format="%I:%M %p").dt.strftime(
-            "%H:%M"
-        )
-    except Exception:
-        story["time"] = story["time"]
-
-    story.convert_dtypes()
-
-    # Parsing the date and time into columns.
-    story["year"] = pd.DatetimeIndex(story["date"]).year
-    story["month"] = pd.DatetimeIndex(story["date"]).month
-    story["day"] = pd.DatetimeIndex(story["date"]).day
-    story["hour"] = pd.DatetimeIndex(story["time"]).hour
-    story["minute"] = pd.DatetimeIndex(story["time"]).minute
-
-    # append story to raw stories df to be merged with historical df
-    stories = pd.concat([stories, story], ignore_index=True)
-
+    dicts.append(article_dict)
     count += 1
-
-# Creating a new dataframe with the columns headline, timestamp, year, month, day,
-# hour, and minute.
-stories = stories[["headline", "timestamp", "year", "month", "day", "hour", "minute"]]
+current = pd.DataFrame(dicts)
 
 
 # This is reading the headlines.csv file and converting it to a dataframe.
 existing = pd.read_csv("headlines.csv")
 existing = existing.dropna(axis=1).set_index("Unnamed: 0")
-existing.convert_dtypes()
+if "tags" not in existing:
+    existing["tags"] = None
+existing
 
-# Parsing the timestamp into columns.
-existing["year"] = pd.DatetimeIndex(existing["timestamp"]).year
-existing["month"] = pd.DatetimeIndex(existing["timestamp"]).month
-existing["day"] = pd.DatetimeIndex(existing["timestamp"]).day
-existing["hour"] = pd.DatetimeIndex(existing["timestamp"]).hour
-existing["minute"] = pd.DatetimeIndex(existing["timestamp"]).minute
 
-# This is a try/except block. It is trying to read the headlines.csv file and
-# convert it to a dataframe with time-parsed columns. If it fails, it will pass.
-try:
-    existing = pd.read_csv("headlines.csv")
-    existing.convert_dtypes()
-    existing["year"] = pd.DatetimeIndex(existing["timestamp"]).year
-    existing["month"] = pd.DatetimeIndex(existing["timestamp"]).month
-    existing["day"] = pd.DatetimeIndex(existing["timestamp"]).day
-    existing["hour"] = pd.DatetimeIndex(existing["timestamp"]).hour
-    existing["minute"] = pd.DatetimeIndex(existing["timestamp"]).minute
-    existing = existing[
-        [
-            "headline",
-            "timestamp",
-            "date",
-            "time",
-            "year",
-            "month",
-            "day",
-            "hour",
-            "minute",
-        ]
-    ]
-except Exception:
-    pass
+dfs = [current, existing]
+existing = existing[
+    ["headline", "tags", "timestamp", "year", "month", "day", "hour", "minute"]
+]
+export = pd.concat([current, existing])
 
-# This is concatenating the existing dataframe with the new dataframe.
-export = pd.concat([existing, stories], ignore_index=True)
-export = export[["headline", "timestamp", "year", "month", "day", "hour", "minute"]]
-
-# Dropping duplicates and sorting the dataframe by year, month, day, hour, and
-# minute.
-export = export.drop_duplicates(subset=["timestamp"], keep="last")
 export = export.sort_values(
     by=["year", "month", "day", "hour", "minute"],
     ascending=[False, False, False, False, False],
     ignore_index=True,
 )
+export = export.drop_duplicates(subset=["timestamp"], keep="first", ignore_index=True)
+
 
 # Writing the dataframe to a csv file.
 export.to_csv("headlines.csv")
