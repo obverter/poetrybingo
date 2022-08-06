bb498114cf1535f3e377ff9aec25275fb1682e7c
obverter <ben@obverter.com>
2022-08-06 07:38:54 -0700

    Cleaning up after myself

--- automatic-scraper/scraper.py
+++ automatic-scraper/scraper.py
@@ -6,7 +6,6 @@ import pandas as pd
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
-import re
 import json
 
 
@@ -18,6 +17,8 @@ doc = BeautifulSoup(req.text)
 raw_titles = doc.select("header > a > h2")
 
 
+# This is a for loop that is iterating through the raw_titles list and appending
+# the headlines to the headlines list.
 headlines = []
 valid_punct = ["'", "!", "?"]
 for count, title in enumerate(enumerate(raw_titles)):
@@ -30,6 +31,13 @@ for count, title in enumerate(enumerate(raw_titles)):
 
 
 def get_tags(article, position):
+    """
+    It takes an article and a position, and returns a list of tags for that position
+
+    :param article: the article you want to scrape
+    :param position: The position of the article in the list of articles
+    :return: A list of tags for the article.
+    """
     tags = doc.select("section.tag-cloud > ul > a")
     tag_list = []
     for tag in tags:
@@ -40,19 +48,27 @@ def get_tags(article, position):
 
 
 def break_timestamp(article, position):
+    """
+    It takes the timestamp of an article, breaks it into its component parts, and
+    returns a dictionary with those parts
+
+    :param article: the article you want to scrape
+    :param position: the position of the article in the list of articles
+    :return: A dictionary with the calendar, clock, year, month, day, hour, and
+    minute.
+    """
     timestamps = doc.select(".article")
     timestamp = timestamps[position].text.split("PT")[-20:]
     timestamp = timestamp[0][-20:]
     timestamp = timestamp.strip()
     calendar = timestamp[:9]
     clock = timestamp[-8:].strip()
-    date_a_frame = pd.DataFrame()
     year = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").year
     month = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").month
     day = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").day
     hour = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").hour
     minute = datetime.strptime(timestamp, "%m/%d/%Y %I:%M %p").minute
-    datetimes = {
+    return {
         "calendar": calendar,
         "clock": clock,
         "year": year,
@@ -62,20 +78,16 @@ def break_timestamp(article, position):
         "minute": minute,
     }
 
-    meta = [datetimes]
-    return datetimes
-
 
 break_timestamp(raw_titles, 3)
 
 
-# init dataframe of tags
+# Creating a list of tags for each article.
 tags = doc.select("section.tag-cloud > ul > a")
 
+# Creating a list of dictionaries.
 valid_punct = ["'", "!", "?"]
-
 dicts = []
-
 count = 0
 for article_position, article in enumerate(raw_titles, start=1):
     headline = raw_titles[count].get_text().strip().replace("\n", " ") + "."
@@ -85,6 +97,7 @@ for article_position, article in enumerate(raw_titles, start=1):
 
     datetimes = break_timestamp(article, count)
     timestamps = doc.select(".article")
+
     # grab, clean timestamp
     timestamp = timestamps[count].text.split("PT")[-20:]
     timestamp = timestamp[0][-20:]
@@ -106,18 +119,20 @@ current = pd.DataFrame(dicts)
 
 
 # This is reading the headlines.csv file and converting it to a dataframe.
-existing = pd.read_csv("headlines.csv", index_col='Unnamed: 0')
+existing = pd.read_csv("headlines.csv", index_col="Unnamed: 0")
 if "tags" not in existing:
     existing["tags"] = None
-existing
 
 
+# Creating a list of dataframes, and then concatenating them.
 dfs = [current, existing]
 existing = existing[
     ["headline", "tags", "timestamp", "year", "month", "day", "hour", "minute"]
 ]
 export = pd.concat([current, existing])
 
+# Sorting the dataframe by the year, month, day, hour, and minute, and then
+# dropping the duplicates.
 export = export.sort_values(
     by=["year", "month", "day", "hour", "minute"],
     ascending=[False, False, False, False, False],
